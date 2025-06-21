d78defbadb308e271d3e254c836b458e12953f6a
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2025-06-21 22:36:09 +0200

    Adding styled components

--- src/Square/index.js
+++ src/Square/index.js
@@ -1,3 +1,5 @@
+import { SquareButton } from "./styled";
+
 export default function Square({ value, onSquareClick }) {
-    return <button className="square" onClick={onSquareClick}>{value}</button>;
-}
+    return <SquareButton onClick={onSquareClick}>{value}</SquareButton>;
+};
---
+++ src/Square/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const SquareButton = styled.button`
+  background-color: #ffff;
+  border: 1px solid #9999;
+  float: left;
+  font-size: 50px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 100px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 33vw;
+`;
